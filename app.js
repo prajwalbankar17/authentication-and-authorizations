6a319be4c6410bc16de8367c43fd66fcc3ea8bbb
prajwalbankar17 <prajwalbankar11@gmail.com>
2025-07-25 12:42:40 +0530

    Encrypt the password

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
+const bcrypt = require("bcrypt");
 
 app.get("/", function (req, res) {
   res.cookie("name", "prajwal");
   res.send("done");
+  bcrypt.genSalt(10, function (err, salt) {
+    bcrypt.hash("prajwal", salt, function (err, hash) {
+      console.log(hash);
+    });
+  });
 });
-
 app.listen(3000);
