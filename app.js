77fb58c41ad5bb6c3b18cf90457e651a8ddac662
prajwalbankar17 <prajwalbankar11@gmail.com>
2025-07-25 14:55:09 +0530

    JWT creation

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
-const express = require("express");
+// const express = require("express");
+// const app = express();
+// const bcrypt = require("bcrypt");
+
+// app.get("/", function (req, res) {
+//   res.cookie("name", "prajwal");
+//   res.send("done");
+//   bcrypt.genSalt(10, function (err, salt) {
+//     bcrypt.hash("prajwal", salt, function (err, hash) {
+//       console.log(hash);
+//     });
+//   });
+// });
+// app.listen(3000);
+
+
+
+
+const express = require('express');
 const app = express();
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
 
 app.get("/", function (req, res) {
-  res.cookie("name", "prajwal");
-  res.send("done");
-  bcrypt.genSalt(10, function (err, salt) {
-    bcrypt.hash("prajwal", salt, function (err, hash) {
-      console.log(hash);
-    });
-  });
-});
+    let token = jwt.sign({email: "prajwal@gmail.com"}, "secret");
+    res.cookie("token", token);
+    res.send("done")
+})
+
+app.get('/read', function (req, res) {
+   console.log(req.cookies.token);
+})
+
 app.listen(3000);
